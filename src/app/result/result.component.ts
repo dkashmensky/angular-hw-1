537b214852d591ad0b8ad228baa49362a3ad3bf4
unknown <d.kashmensky@gmail.com>
2020-04-19 18:07:42 +0300

    Add styles and clean up bugs

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ResultComponent {
   @Input() username: string;
@@ -28,27 +29,28 @@ export class ResultComponent {
     }
 
     const highscores = JSON.parse(scores)
-      .sort((a, b) => b.score - a.score)
+      .sort((a, b) => b.score / b.timer - a.score / a.timer)
+      .slice(0, 10)
       .map((item) => {
         return `
         <tr>
           <td>${item.user}</td>
           <td>${item.score}</td>
           <td>${item.timer}</td>
-          <td>${new Date(item.timestamp).toLocaleString()}</td>
+          <td>${Math.round((item.score / item.timer) * 100) / 100}</td>
         </tr>
         `;
       })
       .join('');
 
     return `
-    <table>
+    <table class="highscores__table">
       <thead>
         <tr>
           <th>Name</th>
           <th>Score</th>
           <th>Game timer</th>
-          <th>Game date</th>
+          <th>Clicks per second</th>
         </tr>
       </thead>
       <tbody>
